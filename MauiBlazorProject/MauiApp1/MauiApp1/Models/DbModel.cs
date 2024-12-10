ec45b8172d92b9ef23cbf0f2a25350fd10cdbc71
TusherAhmed8 <112817249+TusherAhmed8@users.noreply.github.com>
2024-12-10 16:35:28 +0600

    Maui

---
+++ MauiBlazorProject/MauiApp1/MauiApp1/Models/DbModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Models
+{
+    internal class ProductCategory
+    {
+        public int ProductCategoryID { get; set; }
+        public string Name { get; set; } = default!;
+        public List<Product>? Products { get; set; }
+    }
+    public class Product
+    {
+
+        public int ProductID { get; set; }
+
+
+        public string Name { get; set; } = default!;
+
+
+        public string ProductNumber { get; set; } = default!;
+
+        public string Color { get; set; } = default!;
+
+
+        public decimal StandardCost { get; set; }
+
+
+        public decimal ListPrice { get; set; }
+
+
+        public int Size { get; set; }
+
+
+        public decimal Weight { get; set; }
+
+
+        public int ProductCategoryID { get; set; }
+    }
+}
